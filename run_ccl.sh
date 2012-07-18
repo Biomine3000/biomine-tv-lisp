dae467a500800f205995eef6e84e1a0f5d306944
Atte Hinkka <atte.hinkka@iki.fi>
2012-07-18 19:36:15 +0300

    Update README, clean up extra files

--- run_ccl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dx86cl --load ./biomine-tv --eval '(progn (biomine-tv::main "localhost" 7890))'
--- tv-libs.lisp
+++
@@ -1,6 +0,0 @@
-;;;;
-;;;; Helper to load library dependencies for core
-;;;;
-(ql:quickload '("usocket" "cl-json" "bordeaux-threads" "flexi-streams"
-		"cl-ppcre" "mcclim" "ironclad" "babel" "mcclim"))
-(sb-ext:save-lisp-and-die "tv.core" :purify t :compression t)
